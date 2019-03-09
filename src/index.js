3efb5e1649b659969e16dd90e9136640a5c61baa
annatsu <brendonpagano@gmail.com>
2019-03-09 00:45:28 -0300

    Initialize the development with create-react-app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+    <div>Hello, World! I'm working perfectly.</div>,
+    document.getElementById('root'),
+);
